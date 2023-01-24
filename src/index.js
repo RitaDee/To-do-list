e75be954dc7eac1c0d2996c42879fa95e12499e2
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-01-24 16:49:08 +0100

    Add content to HTML file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
 import './style.css';
+
+const array = [
+    {
+        description: 'Wash plate',
+        completed: false,
+        index: 0,
+    },
+    {
+        description: 'Wash plate',
+        completed: false,
+        index: 0,
+    },
+    {
+        description: 'Wash plate',
+        completed: false,
+        index: 0,
+    }
+];
+
+const guestSpeakers = document.querySelector('.speakers-container');
+
+for (let i = 0; i < speakers.length; i += 1) {
+  let visibleImg = 'show';
+  if (i > 1) {
+    visibleImg = 'hide';
+  }
+  guestSpeakers.innerHTML += `
+  <div class="${visibleImg} speakers" id="speak">
+    <div><img src="${speakers[i].speakerImg}" alt="image"></div>
+            <div class="speaker-info">
+            <h3>${speakers[i].speakerName}</h3>
+            <h4>${speakers[i].speakerPost}</h4>
+            <span class="speaker-bar"></span>
+            <p>${speakers[i].speakerInfo}</p>         
+             </div>
+             </div>`;
+}
