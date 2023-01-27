d4e8632edd16bc5e8be404ed9a3742005685b068
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-01-27 15:11:29 +0100

    Add interactive list

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './style.css';
 import Tasks from './modules/task.js';
+import Status from './modules/status.js';
 
 const addBtn = document.querySelector('#add-btn');
 addBtn.addEventListener('click', (event) => {
@@ -11,4 +12,20 @@ addBtn.addEventListener('click', (event) => {
 
 // render the tasks from local storage on page load
 const tasks = new Tasks();
-tasks.render();
+const status = new Status(tasks);
+window.onload = tasks.render();
+
+const checkboxes = document.getElementsByClassName('checkbox-tick');
+Array.from(checkboxes).forEach((checkbox) => {
+  checkbox.addEventListener('change', (event) => {
+    const index = parseInt(event.target.parentElement.parentElement.getAttribute('data-index'), 10);
+    status.updateStatus(index);
+    tasks.render();
+  });
+});
+
+const clearCompletedBtn = document.querySelector('.clear-completed');
+clearCompletedBtn.addEventListener('click', () => {
+  status.clearCompleted();
+  tasks.render();
+});
---
+++ src/modules/status.js
@@ -0,0 +1,28 @@
+export default class Status {
+  constructor(task) {
+    this.task = task;
+    this.array = JSON.parse(localStorage.getItem('array')) || [];
+    this.updateStatus = this.updateStatus.bind(this);
+    this.clearCompleted = this.clearCompleted.bind(this);
+  }
+
+  updateStatus(index) {
+    const updatedArray = this.array.map((arr) => (arr.index === index
+      ? { ...arr, completed: !arr.completed }
+      : { ...arr }));
+    localStorage.setItem('array', JSON.stringify(updatedArray));
+    window.location.reload();
+    this.task.render();
+  }
+
+  clearCompleted() {
+    this.array = JSON.parse(localStorage.getItem('array')) || [];
+    this.array = this.array.filter((task) => task.completed === false);
+    this.array.forEach((task, i) => {
+      task.index = i + 1;
+    });
+    localStorage.setItem('array', JSON.stringify(this.array));
+    window.location.reload();
+    this.task.render();
+  }
+}
